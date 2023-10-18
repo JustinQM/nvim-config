26509b821c091d41cada6b58b74d9e1962213018
Justin <jumpingbeanlittlefoot@gmail.com>
2023-10-17 22:21:51 -0700

    added bootstrapping (might have messed things up)

--- init.lua
+++ init.lua
@@ -12,11 +12,12 @@ vim.g.maplocalleader = ","
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+
 -- IMPORTS
+require('plug')      -- Plugins
 require('vars')      -- Variables
 require('opts')      -- Options
 require('keys')      -- Keymaps
-require('plug')      -- Plugins
 
 --filebrowser
 require("nvim-tree").setup({  
