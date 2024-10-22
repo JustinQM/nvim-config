0f5b1e944eb326e7e32beb41790fe8acdb2f6b69
Justin O'Reilly <jumpingbeanlittlefoot@gmail.com>
2024-10-21 17:44:19 -0700

    deleted old config files

--- lua/lsp.lua
+++
@@ -1,6 +0,0 @@
-local lspconfig = require('lspconfig')
-local coq = require('coq') 
-
-lspconfig.clangd.setup(coq.lsp_ensure_capabilities())
-lspconfig.pyright.setup(coq.lsp_ensure_capabilities())
-
